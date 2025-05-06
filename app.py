bc6d5dce22a8770c2850ef5525a9dc26a7214e90
GoldenArt-IT <store.goldenart@gmail.com>
2025-05-06 17:58:19 +0800

    feat : add metric to see how many staff is available in production department

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ df_outfilter_gatepass = df[df['GATEPASS OUT'].isna() | (df['GATEPASS OUT'] == ''
 df_outfilter_gatepass = df_outfilter_gatepass[df_outfilter_gatepass['GATEPASS IN'].isna() | (df_outfilter_gatepass['GATEPASS IN'] == '')]
 clean_df = df.drop(['E LEAVE RECORDS', 'GATEPASS OUT', 'GATEPASS IN', 'DIVISION'], axis=1)
 
+staff_data_df = st.connection("staffdata", type=GSheetsConnection).read(worksheet="STAFF DATA", ttl=3000)
+staff_data_df = staff_data_df[(staff_data_df['WORK STATUS'] == 'NO STATUS')]
+staff_data_df = staff_data_df[['DEPARTMENT', 'STAFF NAME' ]]
 
 st.title("GA E-Leave Calendar 📅")
 
@@ -90,6 +93,23 @@ with col2:
 
         # displayListAbsent()
         st.header(f"Staff Leave on {sel_date}")
+        col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12 = st.columns(12)
+        staff_available_df = staff_data_df[~staff_data_df['STAFF NAME'].isin(df_absent['STAFF NAME'])]
+        staff_available_df = staff_available_df.groupby(by=['DEPARTMENT']).count()
+        staff_groupby_df = staff_data_df.groupby(by=['DEPARTMENT']).count()
+
+        col1.metric(label="WELDER", value=staff_available_df.loc['WELDER'], delta=int(staff_available_df.loc['WELDER'].values) - int(staff_groupby_df.loc['WELDER'].values))
+        col2.metric(label="INTERIOR", value=staff_available_df.loc['INTERIOR'], delta=int(staff_available_df.loc['INTERIOR'].values) - int(staff_groupby_df.loc['INTERIOR'].values))
+        col3.metric(label="FRAME", value=staff_available_df.loc['FRAME'], delta=int(staff_available_df.loc['FRAME'].values) - int(staff_groupby_df.loc['FRAME'].values))
+        col4.metric(label="FABRIC", value=staff_available_df.loc['FABRIC'], delta=int(staff_available_df.loc['FABRIC'].values) - int(staff_groupby_df.loc['FABRIC'].values))
+        col5.metric(label="SEWING", value=staff_available_df.loc['SEWING'], delta=int(staff_available_df.loc['SEWING'].values) - int(staff_groupby_df.loc['SEWING'].values))
+        col6.metric(label="SPONGE", value=staff_available_df.loc['SPONGE'], delta=int(staff_available_df.loc['SPONGE'].values) - int(staff_groupby_df.loc['SPONGE'].values))
+        col7.metric(label="SPRAY", value=staff_available_df.loc['SPRAY'], delta=int(staff_available_df.loc['SPRAY'].values) - int(staff_groupby_df.loc['SPRAY'].values))
+        col8.metric(label="ASSEMBLY", value=staff_available_df.loc['ASSEMBLY'], delta=int(staff_available_df.loc['ASSEMBLY'].values) - int(staff_groupby_df.loc['ASSEMBLY'].values))
+        col9.metric(label="PACKING", value=staff_available_df.loc['PACKING'], delta=int(staff_available_df.loc['PACKING'].values) - int(staff_groupby_df.loc['PACKING'].values))
+        col10.metric(label="OUTDOOR", value=staff_available_df.loc['OUTDOOR'], delta=int(staff_available_df.loc['OUTDOOR'].values) - int(staff_groupby_df.loc['OUTDOOR'].values))
+        col11.metric(label="R&D", value=staff_available_df.loc['R&D'], delta=int(staff_available_df.loc['R&D'].values) - int(staff_groupby_df.loc['R&D'].values))
+
         df_absent
 
         st.header(f"Gatepass on {sel_date}")
