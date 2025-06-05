011de2544dcd8bf98ed0f5e9f7e20958a0d0ae7f
GoldenArt-IT <store.goldenart@gmail.com>
2025-06-05 12:24:22 +0800

    feat : add session state for date selection for selection main constant at all time

--- app.py
+++ app.py
@@ -58,23 +58,31 @@ cal_options = {
 }
 col1, col2 = st.columns([1,1.5])
 
+# add session state to store calendar click events
+if 'leave_cal' not in st.session_state:
+    st.session_state.leave_cal = {}
+elif 'eventClick' not in st.session_state.leave_cal:
+    st.session_state.leave_cal = {}
+elif st.session_state.leave_cal["eventClick"]["event"] != None: 
+    st.session_state.leave_cal = st.session_state.leave_cal["eventClick"]["event"]
+
+
 with col1:
     cal_ret = calendar(key="leave_cal", options=cal_options, events=events)
-    # cal_ret
+    with st.expander("Debug"):
+        st.session_state.leave_cal
 
 with col2:
     ## extract ISO date when user clicks a day
-    if cal_ret and cal_ret.get("callback") == "eventClick":
+    if st.session_state.leave_cal != {}:
         ## e.g. "2025-05-29T00:00:00.000Z" → "05/29/2025"
-        sel_date = cal_ret["eventClick"]["event"]
+        sel_date = st.session_state.leave_cal
         
         sel_date = pd.to_datetime(sel_date.get('start')).strftime('%m/%d/%Y')
         # text = st.write("yahoo")
 
         df['LEAVE ON'] = pd.to_datetime(df["LEAVE ON"]).dt.strftime('%m/%d/%Y')
         df['LEAVE UNTIL'] = pd.to_datetime(df["LEAVE UNTIL"]).dt.strftime('%m/%d/%Y')
-
-
 
         mask = (df['LEAVE ON'] <= sel_date) & (df['LEAVE UNTIL'] >= sel_date)
 
@@ -90,30 +98,30 @@ with col2:
         # @st.dialog(f"Staff Absent on {sel_date}")
         # def displayListAbsent():
         #     df_absent
-
         # displayListAbsent()
+
         st.header(f"Staff Leave on {sel_date}")
         col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12 = st.columns(12)
         staff_available_df = staff_data_df[~staff_data_df['STAFF NAME'].isin(df_absent['STAFF NAME'])]
         staff_available_df = staff_available_df.groupby(by=['DEPARTMENT']).count()
         staff_groupby_df = staff_data_df.groupby(by=['DEPARTMENT']).count()
 
-        col1.metric(label="WELDER", value=staff_available_df.loc['WELDER'], delta=int(staff_available_df.loc['WELDER'].values) - int(staff_groupby_df.loc['WELDER'].values))
-        col2.metric(label="INTERIOR", value=staff_available_df.loc['INTERIOR'], delta=int(staff_available_df.loc['INTERIOR'].values) - int(staff_groupby_df.loc['INTERIOR'].values))
-        col3.metric(label="FRAME", value=staff_available_df.loc['FRAME'], delta=int(staff_available_df.loc['FRAME'].values) - int(staff_groupby_df.loc['FRAME'].values))
-        col4.metric(label="FABRIC", value=staff_available_df.loc['FABRIC'], delta=int(staff_available_df.loc['FABRIC'].values) - int(staff_groupby_df.loc['FABRIC'].values))
-        col5.metric(label="SEWING", value=staff_available_df.loc['SEWING'], delta=int(staff_available_df.loc['SEWING'].values) - int(staff_groupby_df.loc['SEWING'].values))
-        col6.metric(label="SPONGE", value=staff_available_df.loc['SPONGE'], delta=int(staff_available_df.loc['SPONGE'].values) - int(staff_groupby_df.loc['SPONGE'].values))
-        col7.metric(label="SPRAY", value=staff_available_df.loc['SPRAY'], delta=int(staff_available_df.loc['SPRAY'].values) - int(staff_groupby_df.loc['SPRAY'].values))
-        col8.metric(label="ASSEMBLY", value=staff_available_df.loc['ASSEMBLY'], delta=int(staff_available_df.loc['ASSEMBLY'].values) - int(staff_groupby_df.loc['ASSEMBLY'].values))
-        col9.metric(label="PACKING", value=staff_available_df.loc['PACKING'], delta=int(staff_available_df.loc['PACKING'].values) - int(staff_groupby_df.loc['PACKING'].values))
-        col10.metric(label="OUTDOOR", value=staff_available_df.loc['OUTDOOR'], delta=int(staff_available_df.loc['OUTDOOR'].values) - int(staff_groupby_df.loc['OUTDOOR'].values))
-        col11.metric(label="R&D", value=staff_available_df.loc['R&D'], delta=int(staff_available_df.loc['R&D'].values) - int(staff_groupby_df.loc['R&D'].values))
+        col1.metric(label="WELDER", value=staff_available_df.loc['WELDER'], delta=int(staff_available_df.loc['WELDER'].values[0]) - int(staff_groupby_df.loc['WELDER'].values[0]))
+        col2.metric(label="INTERIOR", value=staff_available_df.loc['INTERIOR'], delta=int(staff_available_df.loc['INTERIOR'].values[0]) - int(staff_groupby_df.loc['INTERIOR'].values[0]))
+        col3.metric(label="FRAME", value=staff_available_df.loc['FRAME'], delta=int(staff_available_df.loc['FRAME'].values[0]) - int(staff_groupby_df.loc['FRAME'].values[0]))
+        col4.metric(label="FABRIC", value=staff_available_df.loc['FABRIC'], delta=int(staff_available_df.loc['FABRIC'].values[0]) - int(staff_groupby_df.loc['FABRIC'].values[0]))
+        col5.metric(label="SEWING", value=staff_available_df.loc['SEWING'], delta=int(staff_available_df.loc['SEWING'].values[0]) - int(staff_groupby_df.loc['SEWING'].values[0]))
+        col6.metric(label="SPONGE", value=staff_available_df.loc['SPONGE'], delta=int(staff_available_df.loc['SPONGE'].values[0]) - int(staff_groupby_df.loc['SPONGE'].values[0]))
+        col7.metric(label="SPRAY", value=staff_available_df.loc['SPRAY'], delta=int(staff_available_df.loc['SPRAY'].values[0]) - int(staff_groupby_df.loc['SPRAY'].values[0]))
+        col8.metric(label="ASSEMBLY", value=staff_available_df.loc['ASSEMBLY'], delta=int(staff_available_df.loc['ASSEMBLY'].values[0]) - int(staff_groupby_df.loc['ASSEMBLY'].values[0]))
+        col9.metric(label="PACKING", value=staff_available_df.loc['PACKING'], delta=int(staff_available_df.loc['PACKING'].values[0]) - int(staff_groupby_df.loc['PACKING'].values[0]))
+        col10.metric(label="OUTDOOR", value=staff_available_df.loc['OUTDOOR'], delta=int(staff_available_df.loc['OUTDOOR'].values[0]) - int(staff_groupby_df.loc['OUTDOOR'].values[0]))
+        col11.metric(label="R&D", value=staff_available_df.loc['R&D'], delta=int(staff_available_df.loc['R&D'].values[0]) - int(staff_groupby_df.loc['R&D'].values[0]))
 
-        df_absent
+        st.table(df_absent)
 
         st.header(f"Gatepass on {sel_date}")
-        df_gatepass
+        st.table(df_gatepass)
 
     else:
         st.header(f"All Leave Data")
